ced135e08c498815908c0515b13a6660853bd156
Rafael <rafaeljuliani1984@gmail.com>
2020-12-07 15:15:48 -0300

    feat: Validacao convertida em funçao

--- calculo.sh
+++ calculo.sh
@@ -7,26 +7,28 @@
 #  Expressão regular para números inteiros.
 regex='^[0-9]+$'
 
+#  Estrutura if que verifica se a variável recebida é um inteiro.
+#  Se não for, o programa encerra.
+#  Obs: O operador =~ é usado  para testar uma variável em relação a uma expressão regular.
+validacao() {
+  if ! [[ $1 =~ $regex ]] ; then
+    echo "erro: \"${1}\" não é um número inteiro" ; exit 1
+  fi
+}
+
 #  Leitura da primeira variável.
 echo -n "Informe o primeiro número inteiro e pressione [ENTER]: "
 read int1
 
-#  Estrutura if que verifica se a primeira variável recebida é um inteiro.
-#  Se não for, o programa encerra.
-#  Obs: O operador =~ é usado  para testar uma variável em relação a uma expressão regular.
-if ! [[ $int1 =~ $regex ]] ; then
-  echo "erro: \"${int1}\" não é um número inteiro" ; exit 1
-fi
+#  Validação da primeira varíavel.
+validacao $int1
 
 #  Leitura da segunda variável.
 echo -n "Informe o segundo número inteiro e pressione [ENTER]: "
 read int2
 
-#  Estrutura if que verifica se a segunda variável recebida é um inteiro.
-#  Se não for, o programa encerra.
-if ! [[ $int2 =~ $regex ]] ; then
-  echo "erro: \"${int2}\" não é um número inteiro" ; exit 1
-fi
+#  Validação da segunda varíavel.
+validacao $int2
 
 #  Declaração das variáveis que realizam as operações.
 let soma=int1+int2
