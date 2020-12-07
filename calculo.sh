6d5f0d84d065879b1a1188036bf344a60072ec00
Rafael <rafaeljuliani1984@gmail.com>
2020-12-07 15:11:21 -0300

    docs: Operaçoes matematicas

--- calculo.sh
+++ calculo.sh
@@ -12,6 +12,7 @@ echo -n "Informe o primeiro número inteiro e pressione [ENTER]: "
 read int1
 
 #  Estrutura if que verifica se a primeira variável recebida é um inteiro.
+#  Se não for, o programa encerra.
 #  Obs: O operador =~ é usado  para testar uma variável em relação a uma expressão regular.
 if ! [[ $int1 =~ $regex ]] ; then
   echo "erro: \"${int1}\" não é um número inteiro" ; exit 1
@@ -22,6 +23,7 @@ echo -n "Informe o segundo número inteiro e pressione [ENTER]: "
 read int2
 
 #  Estrutura if que verifica se a segunda variável recebida é um inteiro.
+#  Se não for, o programa encerra.
 if ! [[ $int2 =~ $regex ]] ; then
   echo "erro: \"${int2}\" não é um número inteiro" ; exit 1
 fi
